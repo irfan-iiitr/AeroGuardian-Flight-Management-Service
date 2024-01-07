8013b70349f2617c34ea248ad7eb43b66fa4c62f
irfan-iiitr <imirfansari@gmail.com>
2024-01-07 11:06:34 +0530

    authServie added

---
+++ AuthService/src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+
+// const {User} = require('./models/index');
+// const bcrypt = require('bcrypt');
+
+const app = express();
+
+const db= require('./models/index');
+
+const prepareAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server Started on Port: ${PORT}`);
+        // const incomingpassword = '123456';
+        // const user = await User.findByPk(3);
+        // const response = bcrypt.compareSync(incomingpassword, user.password);
+        // console.log(response);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+
+        
+    });
+}   
+
+
+prepareAndStartServer();
